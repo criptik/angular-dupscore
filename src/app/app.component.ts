b0d80eb57ac8442ff6d92104cbb926d17f1f987c
Tom Deneau <tom.deneau@gmail.com>
2023-03-01 17:26:14 -0600

    first cut scoring grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+    myTitle:string = 'my-app';
+    myPhrase:string = 'this is funny';
+    undefScore: number = 9999;
+    blanks: string = '  ';
+    myArray:(number)[] = [100,-200,50, this.undefScore, -420];
+
+    nsString(nsScore:number): string {
+        return (nsScore >=0 && nsScore !== this.undefScore? this.stringFor(nsScore) : this.blanks);
+    }
+    ewString(nsScore:number): string {
+        return (nsScore <0 && nsScore !== this.undefScore ? this.stringFor(-1*nsScore) : this.blanks);
+    }
+    stringFor(score:number): string {
+        return (score.toString()); 
+    }
+    
 }
+

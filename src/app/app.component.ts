dfb7641a398c9fd9acdb24256d1df6189aadc18b
Tom Deneau <tom.deneau@gmail.com>
2023-03-03 16:44:26 -0600

    score page starting refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-const undefScore: number = 9999;
+const undefScore: number = -1;
 
 
 @Component({
@@ -12,23 +12,40 @@ const undefScore: number = 9999;
 export class AppComponent {
     myTitle:string = 'my-app';
     myPhrase:string = 'this is funny';
-    blanks: string = '  ';
-    myArray: ScoreLine[] = [
-        new ScoreLine(3,4,1100),
-        new ScoreLine(5,2,-1100),
+    numPairs: number = 10;
+    myLines: string[] = [];
+    headerLine: string = `  NS    SCORE    EW`;
+    undefStr: string = '. .';
+    scoreLineArray: ScoreLine[] = [
+        new ScoreLine(5,2,-1),
+        new ScoreLine(3,4, 1100),
         new ScoreLine(6,1,-1100),
-       ];
+        new ScoreLine(8,7, 0),
+    ];
 
-    nsString(nsScore:number): string {
-        return (nsScore >=0 && nsScore !== undefScore? this.stringFor(nsScore) : this.blanks);
+    constructor() {
+        this.updateView();
     }
-    ewString(nsScore:number): string {
-        return (nsScore <0 && nsScore !== undefScore ? this.stringFor(-1*nsScore) : this.blanks);
+
+    updateView() {
+        [...Array(this.numPairs).keys()].forEach(pair => {
+            this.myLines[pair+1] = `      ${'--'.repeat(5)}   `; 
+        });
+        this.scoreLineArray.forEach(line => {
+            this.myLines[line.nsPair] = `  ${line.nsPair.toString().padStart(2,' ')}  ${this.scoreStr(line.score, true)} ${this.scoreStr(line.score, false)}  ${line.ewPair.toString().padStart(2,' ')}    `;
+            console.log(this.myLines[line.nsPair]);
+        });
+        
     }
-    stringFor(score:number): string {
-        return (score.toString()); 
+
+    scoreStr(score: number, forNS: boolean): string {
+        var str = ' ';
+        if (score === undefScore) str = ' ? ';
+        else if (score === 0 && forNS) str = 'PASS';
+        else if (score > 0 && forNS) str = `${score}`;
+        else if (score < 0 && !forNS) str = `${-1*score}`;
+        return str.padStart(4, ' ');
     }
-    
 }
 
 interface ScoreLine {

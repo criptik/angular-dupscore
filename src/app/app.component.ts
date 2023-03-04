2299775a57753a7429106bdd5bfff6cf0de05cd7
Tom Deneau <tom.deneau@gmail.com>
2023-03-04 16:29:40 -0600

    input line and onNS arrow added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-const undefScore: number = -1;
-
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -12,58 +9,57 @@ const undefScore: number = -1;
 export class AppComponent {
     myTitle:string = 'my-app';
     myPhrase:string = 'this is funny';
-    numPairs: number = 10;
-    myLines: string[] = [];
-    headerLine: string = `  NS    SCORE    EW`;
-    undefStr: string = '. .';
-    scoreLineArray: ScoreLine[] = [
-        new ScoreLine(5,2,-1),
-        new ScoreLine(3,4, 1100),
-        new ScoreLine(6,1,-1100),
-        new ScoreLine(8,7, 0),
-    ];
-
+    numPairs: number = 8;
+    viewLines: string[] = [];
+    boardNum: number = 4;
+    boardVul: string = 'BOTH';
+    onNS: number;
+    inputLine: string = ' ';
+    nsScore : Map<number, number> = new Map();
+    nsewMap: Map<number, number> = new Map();
+    
     constructor() {
+        // temporary for testing
+        this.nsScore.set(3, 1100);
+        this.nsScore.set(6, -1100);
+        this.nsScore.set(8, 0);
+        this.nsewMap.set(5, 2);
+        this.nsewMap.set(3, 4);
+        this.nsewMap.set(6, 1);
+        this.nsewMap.set(8, 7);
+        
+        this.onNS = 6;
         this.updateView();
     }
 
     updateView() {
+        this.viewLines[0] = `Section:A  Board:${this.boardNum}  Vul:${this.boardVul}`;
+        this.viewLines[1] = `   NS    SCORE    EW`;
+        // default for unused lines
         [...Array(this.numPairs).keys()].forEach(pair => {
-            this.myLines[pair+1] = `      ${'--'.repeat(5)}   `; 
+            this.viewLines[pair+2] = `      ${'--'.repeat(5)}  `; 
         });
-        this.scoreLineArray.forEach(line => {
-            this.myLines[line.nsPair] = `  ${line.nsPair.toString().padStart(2,' ')}  ${this.scoreStr(line.score, true)} ${this.scoreStr(line.score, false)}  ${line.ewPair.toString().padStart(2,' ')}    `;
-            console.log(this.myLines[line.nsPair]);
+        // handle lines which have real ns&ew pairs (score may still be undefined)
+        Array.from(this.nsewMap.keys()).forEach((nsPair:number) => {
+            const ewPair = this.nsewMap.get(nsPair) as number;
+            const score: number | undefined  = this.nsScore.get(nsPair);
+            const arrow: string = (nsPair === this.onNS ? `==>` : `   `);
+            this.viewLines[nsPair+1] = `${arrow}${nsPair.toString().padStart(2,' ')}  ${this.scoreStr(score, true)} ${this.scoreStr(score, false)}  ${ewPair.toString().padStart(2,' ')}    `;
         });
+        this.viewLines.push(` `);
+        this.viewLines.push(` `);
+        const onEW = this.nsewMap.get(this.onNS) as number;
+        this.inputLine = `Board: ${this.boardNum}  NS:${this.onNS}  EW:${onEW}  VUL:${this.boardVul}     SCORE:`;
         
     }
 
-    scoreStr(score: number, forNS: boolean): string {
+    scoreStr(score: number | undefined, forNS: boolean): string {
         var str = ' ';
-        if (score === undefScore) str = ' ? ';
+        if (score === undefined) str = ' ? ';
         else if (score === 0 && forNS) str = 'PASS';
         else if (score > 0 && forNS) str = `${score}`;
         else if (score < 0 && !forNS) str = `${-1*score}`;
         return str.padStart(4, ' ');
     }
 }
-
-interface ScoreLine {
-    constructor : Function,
-    nsPair : number,
-    ewPair : number,
-    score  : number,
-}
-
-class ScoreLine {
-    constructor(nsPair : number,
-                ewPair : number,
-                score : number = undefScore) {
-        this.nsPair = nsPair;
-        this.ewPair = ewPair;
-        this.score = score;
-    }
-}
-
-
 

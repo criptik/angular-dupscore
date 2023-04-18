50196e615e0819ca911c56dc6ce6f4b084573111
Tom Deneau <tom.deneau@gmail.com>
2023-04-18 12:05:49 -0500

    name-entry using forms checkpoint

--- src/app/names-entry/names-entry.component.ts
+++ src/app/names-entry/names-entry.component.ts
@@ -3,6 +3,7 @@ import { Directive, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { GameDataService, Person, Pair } from '../game-data/game-data.service';
 import { SerializerService } from '../serializer/serializer.service';
+import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import * as _ from 'lodash';
 
 @Component({
@@ -12,10 +13,6 @@ import * as _ from 'lodash';
 })
 export class NamesEntryComponent implements AfterViewInit {
     @ViewChild('nameEntryDialog') nameEntryDialog!: ElementRef<HTMLDialogElement>;
-    @ViewChild('lastName1') lastName1!: ElementRef<HTMLInputElement>;
-    @ViewChild('firstName1') firstName1!: ElementRef<HTMLInputElement>;
-    @ViewChild('lastName2') lastName2!: ElementRef<HTMLInputElement>;
-    @ViewChild('firstName2') firstName2!: ElementRef<HTMLInputElement>;
     nameEntryDialogHeader: string = '';
     pairNumArray: number[] = [];
     pairNameStrArray: string[] = [];
@@ -45,16 +42,21 @@ export class NamesEntryComponent implements AfterViewInit {
     allLastNames: string[] = [];
     nameCompletion: string[] = [];
     lastFirstMap: Map<string, string[]> = new Map();
+    exactArrays: Map<number, string[]> = new Map();
     
+    nameEntryForm = new FormGroup({
+        lastName1:  new FormControl(),
+        firstName1:  new FormControl(),
+        lastName2:  new FormControl(),
+        firstName2:  new FormControl(),
+    });
+
     constructor(private gameDataPtr: GameDataService,
                 private _router: Router,
                 private _activatedRoute: ActivatedRoute,
                 private _serializer: SerializerService, )  {
         // console.log(this.allNames);
         // console.log(this._serializer.serialize(this.allNames));
-    }
-
-    onMyCompleterKeyUp(x: any) {
     }
 
     parseNumberFrom(str: string): number {
@@ -67,6 +69,11 @@ export class NamesEntryComponent implements AfterViewInit {
         // parse number from id which is nameXX
         const pairnum = this.parseNumberFrom(x.target.id);
         this.nameEntryDialogHeader = `Names for Pair ${pairnum}`;
+        this.nameEntryForm.get('lastName1')?.valueChanges.subscribe( curInput => this.genLastNameCompletionList(curInput, 1) );
+        this.nameEntryForm.get('lastName2')?.valueChanges.subscribe( curInput => this.genLastNameCompletionList(curInput, 2) );
+        this.nameEntryForm.get('firstName1')?.valueChanges.subscribe( curInput => this.genFirstNameCompletionList(curInput, 1) );
+        this.nameEntryForm.get('firstName2')?.valueChanges.subscribe( curInput => this.genFirstNameCompletionList(curInput, 2) );
+
         this.nameEntryDialog.nativeElement.showModal();
     }
 
@@ -86,7 +93,7 @@ export class NamesEntryComponent implements AfterViewInit {
         else {
             this.allNames = this._serializer.deserialize(jsonStr, ['Person']);
         }
-        console.log(this.allNames);
+        this.cleanupAllNames();
     }
     
     ngOnInit() {
@@ -119,17 +126,21 @@ export class NamesEntryComponent implements AfterViewInit {
     ngAfterViewInit() {
     }
 
-    genLastNameCompletionList(x: any) {
-        if (x.target.value.length >= 2) {
-            const completionList = this.allLastNames.filter( name => name.toUpperCase().startsWith(x.target.value.toUpperCase()));
+    genLastNameCompletionList(curInput: string, id: number) {
+        // console.log('genLastNameCompletionList', curInput, id);
+        if (curInput === null) return;
+        if (curInput.length >= 2) {
+            const completionList: string[] = this.allLastNames.filter( name =>
+                name.toUpperCase().startsWith(curInput.toUpperCase()));
             this.nameCompletion = completionList;
             // keep track of the cases where there is only one entry
-            const exactArray: string[] = x.target.exactArray ?? [];
+            const exactArray: string[] = this.exactArrays.get(id) ?? [];
             // console.log('exact', exactArray, completionList[0]);
-            if (completionList.length === 1 && !exactArray.includes(completionList[0])) {
-                x.target.value = completionList[0];
+            if (completionList.length === 1 && !exactArray.includes(completionList[0]) && curInput !== completionList[0]) {
+                const control = (id === 1 ? this.nameEntryForm.get('lastName1') : this.nameEntryForm.get('lastName2'));
+                control?.setValue(completionList[0] as never);
                 exactArray.push(completionList[0]);
-                x.target.exactArray = exactArray;
+                this.exactArrays.set(id, exactArray);
             }
         }
         else {
@@ -137,28 +148,19 @@ export class NamesEntryComponent implements AfterViewInit {
         }
     }
     
-    onLastNameInputKeyUp(x: any) {
-        const key: string = x.key;
-        this.genLastNameCompletionList(x);
-    }
-
     onLastNameFocus(x: any) {
-        this.genLastNameCompletionList(x);
+        // console.log('onLastNameFocus', x);
+        this.genLastNameCompletionList(x.target.value, this.parseNumberFrom(x.target.id));
     }
     
-    genFirstNameCompletionList(x: any) {
-        const idx = this.parseNumberFrom(x.target.name);
-        const lastName = (idx === 1 ? this.lastName1.nativeElement.value : this.lastName2.nativeElement.value);
+    genFirstNameCompletionList(curInput: string, id: number) {
+        if (curInput === null) return;
+        const lastName: string = (id === 1 ? this.nameEntryForm.value.lastName1 : this.nameEntryForm.value.lastName2);
         this.nameCompletion = this.lastFirstMap.get(lastName) ?? [];
     }
     
-    onFirstNameInputKeyUp(x: any) {
-        const key: string = x.key;
-        this.genFirstNameCompletionList(x);
-    }
-
     onFirstNameFocus(x: any) {
-        this.genFirstNameCompletionList(x);
+        this.genFirstNameCompletionList(x.target.value, this.parseNumberFrom(x.target.id));
     }
 
     addToAllNames(player: Person) {
@@ -168,19 +170,27 @@ export class NamesEntryComponent implements AfterViewInit {
             this.allNames.push(player);
         }
     }
+
+    cleanupAllNames() {
+        this.allNames = this.allNames.filter( person => person.last !== null && person.first !== null);
+        // console.log(this.allNames);
+    }
     
-    onDialogOKClick(x: any) {
+    onNameEntryFormSubmit() {
         // here we will copy out the fields
         const pairnum = this.parseNumberFrom(this.nameEntryDialogHeader);
-        const playerA: Person = new Person(this.firstName1.nativeElement.value, this.lastName1.nativeElement.value);
-        const playerB: Person = new Person(this.firstName2.nativeElement.value, this.lastName2.nativeElement.value);
+        const playerA: Person = new Person(this.nameEntryForm.value.firstName1, this.nameEntryForm.value.lastName1);
+        const playerB: Person = new Person(this.nameEntryForm.value.firstName2, this.nameEntryForm.value.lastName2);
         const newPair = new Pair(playerA, playerB);
         this.addToAllNames(playerA);
         this.addToAllNames(playerB);
         this.gameDataPtr.pairNameMap.set(pairnum, newPair);
         // console.log(newPair);
+        // clear form fields
+        this.nameEntryForm.reset();
         this.nameEntryDialog.nativeElement.close();
         this.updatePairNameStrArray();
+        this.cleanupAllNames();
         window.localStorage.setItem(this.locStorageKey, this._serializer.serialize(this.allNames));
     }
 }

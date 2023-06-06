b0ad281c492124d118353ece11e74de0856571af
Tom Deneau <tom.deneau@gmail.com>
2023-06-06 17:52:03 -0500

    nav menu uses ngFor, ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,28 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 
 export class AppComponent {
+
+    menuLayout: any[]  = [
+        {text: 'Game',  path: '',  needsGame: false, sub: [
+            {text: 'New',  path: '/setup/new'},
+            {text: 'Load', path: '/setup/load'},
+            {text: 'Delete', path: '/setup/delete'}, ]}, 
+        {text: 'Status',  path: '/status',  needsGame: true},
+        {text: 'Players',  path: '/names',  needsGame: true},
+        {text: 'Score Entry',  path: '/score',  needsGame: true},
+        {text: 'Score Review',  path: '/score-review',  needsGame: true},
+        {text: 'Summary',       path: '/report/short',  needsGame: true},
+        {text: 'Report',        path: '/report/long',  needsGame: true},
+        {text: 'NameData',  path: '',  needsGame: false, sub: [
+            {text: 'Import',  path: '/namedata/import'},
+            {text: 'Edit',    path: '/namedata/edit'},
+            {text: 'Delete',  path: '/namedata/delete'}, ]}, 
+    ];
     
     constructor(
         public gameDataPtr: GameDataService,
         private _router: Router) {    
-        if (!this.gameDataPtr.gameDataSetup) {
+            if (!this.gameDataPtr.gameDataSetup) {
             this._router.navigate(["/setup"]);
         }
         

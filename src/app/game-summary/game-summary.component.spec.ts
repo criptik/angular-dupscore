6eade4813bdbab9a322e61caebce94243d3852f8
Tom Deneau <tom.deneau@gmail.com>
2025-10-19 23:13:59 -0500

    tests for single board details

--- src/app/game-summary/game-summary.component.spec.ts
+++ src/app/game-summary/game-summary.component.spec.ts
@@ -6,8 +6,8 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { provideHttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DeleterDialogComponent } from '../deleter-dialog/deleter-dialog.component';
-import { GameDataService } from '../game-data/game-data.service';
-import { lastValueFrom } from 'rxjs';
+import { GameDataService, BoardObj } from '../game-data/game-data.service';
+import * as _ from 'lodash';
 
 import { GameSummaryComponent } from './game-summary.component';
 
@@ -48,7 +48,6 @@ describe('GameSummaryComponent', () => {
 
     describe('GameSummaryComponent Reports', () => {
         let json251010AStr: string = '';
-        let expectedShortReport: string = '';
         
         beforeEach( (done) => {
             httpClient.get('testAssets/251010A.json', { responseType: 'text' })!
@@ -56,45 +55,92 @@ describe('GameSummaryComponent', () => {
                           // console.log('data1', data1); // 'data' contains the content of your-file.txt
                           // Process the text data here
                           json251010AStr = data1;
-                          // console.log('jsonStr complete');
-                          httpClient.get('testAssets/expectedShortSummary.txt', { responseType: 'text' })!
-                                    .subscribe(data2 => {
-                                        // console.log('data2', data2);
-                                        expectedShortReport = data2;
-                                        done();
-                                    });
+                          done();
                       });
         });
-    
-        it('should have read in files', () => {
-            console.log('jsonLength', json251010AStr.length);
-            expect(json251010AStr.length).not.toBe(0);
-            expect(expectedShortReport.length).not.toBe(0);
-        });
-
-        function jsonToShortReport(jsonStr: string, shouldComputeMP: boolean) {
-            const p: GameDataService = gameDataService;
-            p.doDeserialize(jsonStr);
-            if (shouldComputeMP) {
-                // go thru and recompute the MPs (this should not change anything)
-                Array.from(p.boardObjs.values()).forEach( (board) => {
-                    const boardTop = 2;  // 3 pairs per board for this file
-                    board.computeMP(boardTop);
-                });
+                          
+        describe('GameSummaryComponent Short Reports', () => {
+            let expectedShortReport: string = '';
+            beforeEach( (done) => {
+                httpClient.get('testAssets/expectedShortSummary.txt', { responseType: 'text' })!
+                          .subscribe(data => {
+                              // console.log('data', data); // 'data' contains the content of your-file.txt
+                              // Process the text data here
+                              expectedShortReport = data;
+                              done();
+                          });
+            });
+            
+            function jsonToShortReport(jsonStr: string, shouldComputeMP: boolean) {
+                const p: GameDataService = gameDataService;
+                p.doDeserialize(jsonStr);
+                if (shouldComputeMP) {
+                    // go thru and recompute the MPs (this should not change anything)
+                    Array.from(p.boardObjs.values()).forEach( (board) => {
+                        const boardTop = 2;  // 3 pairs per board for this file
+                        board.computeMP(boardTop);
+                    });
+                }
+                component.size = 'short';
+                component.ngOnInit();
             }
-            component.size = 'short';
-            component.ngOnInit();
-        }
-    
-        it('should produce a correct short report', () => {
-            jsonToShortReport(json251010AStr, false);
-            expect(component.summaryText.trim()).toBe(expectedShortReport.trim());
+            
+            it('should have read in files', () => {
+                console.log('jsonLength', json251010AStr.length);
+                expect(json251010AStr.length).not.toBe(0);
+                expect(expectedShortReport.length).not.toBe(0);
+            });
+
+            it('should produce a correct short report', () => {
+                jsonToShortReport(json251010AStr, false);
+                expect(component.summaryText.trim()).toBe(expectedShortReport.trim());
+            });
+            
+            
+            it('should produce a correct short report after computeMP', () => {
+                jsonToShortReport(json251010AStr,true);
+                expect(component.summaryText.trim()).toBe(expectedShortReport.trim());
+            });
+
         });
 
+        describe('GameSummaryComponent Board Details', () => {
+            let expectedBoardDetails: string = '';
+            let boardDetailsArray: RegExpMatchArray;
 
-        it('should produce a correct short report after computeMP', () => {
-            jsonToShortReport(json251010AStr,true);
-            expect(component.summaryText.trim()).toBe(expectedShortReport.trim());
+            beforeEach( (done) => {
+                httpClient.get('testAssets/expectedBoardDetails.txt', { responseType: 'text' })!
+                          .subscribe(data => {
+                              // console.log('data', data); // 'data' contains the content of your-file.txt
+                              // Process the text data here
+                              expectedBoardDetails = data;
+                              boardDetailsArray = expectedBoardDetails.match(/   RESULTS OF BOARD.+?-{2,}/sg)!;
+                              // console.log('boardDetailsArray:', boardDetailsArray);
+                              done();
+                          });
+            });
+
+            function jsonToOneBoardDetails(jsonStr: string, bdnum: number) : string {
+                const p: GameDataService = gameDataService;
+                p.doDeserialize(jsonStr);
+                component.size = 'long';
+                component.ngOnInit();
+                let pbt: Array<string> = [];
+                const boardObj: BoardObj = p.boardObjs.get(bdnum)!;
+                if (boardObj!.areAnyPlaysEntered()) {
+                    component.outputOneBoardText(pbt, boardObj);
+                }
+                return pbt.join('\n');
+            }
+
+            _.range(20).forEach( (n) => {
+                it(`should produce a correct detailed report for board ${n+1}`, () => {
+                    const details: string = jsonToOneBoardDetails(json251010AStr, n+1);
+                    expect(details.trim()).toBe(boardDetailsArray[n].trim());
+                });
+            });
+            
         });
+        
     });
 });

31081ac3de7bda139dfcb330a8eb7728bc14729c
Tom Deneau <tom.deneau@gmail.com>
2025-10-21 08:02:46 -0500

    add some directory structure to testAssets

--- src/app/game-summary/game-summary.component.spec.ts
+++ src/app/game-summary/game-summary.component.spec.ts
@@ -46,34 +46,47 @@ describe('GameSummaryComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    describe('GameSummaryComponent Reports', () => {
-        let json251010AStr: string = '';
+    describe('3 Table Reports', () => {
+        let jsonStr3Table: string = '';
         
         beforeEach( (done) => {
-            httpClient.get('testAssets/251010A.json', { responseType: 'text' })!
-                      .subscribe(data1 => {
-                          // console.log('data1', data1); // 'data' contains the content of your-file.txt
-                          // Process the text data here
-                          json251010AStr = data1;
+            httpClient.get('testAssets/3table/sample.json', { responseType: 'text' })!
+                      .subscribe(data => {
+                          jsonStr3Table = data;
                           done();
                       });
         });
-                          
-        describe('GameSummaryComponent Short Reports', () => {
-            let expectedShortReport: string = '';
+        
+        describe('3 Table Short Reports', () => {
+            let expectedShort3Table: string = '';
+            let expectedShort3Table2: string = '';
             beforeEach( (done) => {
-                httpClient.get('testAssets/expectedShortSummary.txt', { responseType: 'text' })!
+                httpClient.get('testAssets/3table/expectedShortSummary.txt', { responseType: 'text' })!
                           .subscribe(data => {
-                              // console.log('data', data); // 'data' contains the content of your-file.txt
-                              // Process the text data here
-                              expectedShortReport = data;
+                              expectedShort3Table = data;
+                              done();
+                          });
+            });
+            beforeEach( (done) => {
+                httpClient.get('testAssets/3table/expectedShortSummary2.txt', { responseType: 'text' })!
+                          .subscribe(data => {
+                              expectedShort3Table2 = data;
                               done();
                           });
             });
             
-            function jsonToShortReport(jsonStr: string, shouldComputeMP: boolean) {
+            function jsonToShortReport(jsonStr: string, shouldComputeMP: boolean, modifyBoard5:boolean = false) {
                 const p: GameDataService = gameDataService;
                 p.doDeserialize(jsonStr);
+                if (modifyBoard5) {
+                    const board5: BoardObj = p.boardObjs.get(5)!;
+                    board5.boardPlays.forEach ( (bp) => {
+                        bp.addSpecialScoreInfo('NP');
+                    });
+                    const boardTop = 2;  // 3 pairs per board for this file
+                    board5.computeMP(boardTop);
+                }
+                
                 if (shouldComputeMP) {
                     // go thru and recompute the MPs (this should not change anything)
                     Array.from(p.boardObjs.values()).forEach( (board) => {
@@ -86,35 +99,38 @@ describe('GameSummaryComponent', () => {
             }
             
             it('should have read in files', () => {
-                console.log('jsonLength', json251010AStr.length);
-                expect(json251010AStr.length).not.toBe(0);
-                expect(expectedShortReport.length).not.toBe(0);
+                console.log('jsonLength', jsonStr3Table.length);
+                expect(jsonStr3Table.length).not.toBe(0);
+                expect(expectedShort3Table.length).not.toBe(0);
             });
 
             it('should produce a correct short report', () => {
-                jsonToShortReport(json251010AStr, false);
-                expect(component.summaryText.trim()).toBe(expectedShortReport.trim());
+                jsonToShortReport(jsonStr3Table, false);
+                expect(component.summaryText.trim()).toBe(expectedShort3Table.trim());
             });
             
             
             it('should produce a correct short report after computeMP', () => {
-                jsonToShortReport(json251010AStr,true);
-                expect(component.summaryText.trim()).toBe(expectedShortReport.trim());
+                jsonToShortReport(jsonStr3Table,true);
+                expect(component.summaryText.trim()).toBe(expectedShort3Table.trim());
+            });
+
+            it('should produce a correct short report after board 5 marked 3 NP', () => {
+                jsonToShortReport(jsonStr3Table, false, true);
+                expect(component.summaryText.trim()).toBe(expectedShort3Table2.trim());
             });
 
         });
 
-        describe('GameSummaryComponent Board Details', () => {
-            let expectedBoardDetails: string = '';
+        describe('3 Table Board Details', () => {
+            let expectedDetails3Table: string = '';
             let boardDetailsArray: RegExpMatchArray;
 
             beforeEach( (done) => {
-                httpClient.get('testAssets/expectedBoardDetails.txt', { responseType: 'text' })!
+                httpClient.get('testAssets/3table/expectedDetails.txt', { responseType: 'text' })!
                           .subscribe(data => {
-                              // console.log('data', data); // 'data' contains the content of your-file.txt
-                              // Process the text data here
-                              expectedBoardDetails = data;
-                              boardDetailsArray = expectedBoardDetails.match(/   RESULTS OF BOARD.+?-{2,}/sg)!;
+                              expectedDetails3Table = data;
+                              boardDetailsArray = expectedDetails3Table.match(/   RESULTS OF BOARD.+?-{2,}/sg)!;
                               // console.log('boardDetailsArray:', boardDetailsArray);
                               done();
                           });
@@ -135,12 +151,11 @@ describe('GameSummaryComponent', () => {
 
             _.range(20).forEach( (n) => {
                 it(`should produce a correct detailed report for board ${n+1}`, () => {
-                    const details: string = jsonToOneBoardDetails(json251010AStr, n+1);
+                    const details: string = jsonToOneBoardDetails(jsonStr3Table, n+1);
                     expect(details.trim()).toBe(boardDetailsArray[n].trim());
                 });
             });
             
         });
-        
-    });
+    }); // end of 3-table
 });
